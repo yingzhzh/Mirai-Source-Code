ec05e5579ab1a05897073af8f219dcf1eef5be1b
yingzhzh <753671337@qq.com>
2016-12-23 10:53:56 +0800

    Update admin.go

--- mirai/cnc/admin.go
+++ mirai/cnc/admin.go
@@ -25,7 +25,7 @@ func (this *Admin) Handle() {
         this.conn.Write([]byte("\033[?1049l"))
     }()
 
-    headerb, err := ioutil.ReadFile("prompt.txt")
+    headerb, err := "mirai botnet 僵尸网络"
     if err != nil {
         return
     }
@@ -35,7 +35,7 @@ func (this *Admin) Handle() {
 
     // Get username
     this.conn.SetDeadline(time.Now().Add(60 * time.Second))
-    this.conn.Write([]byte("\033[34;1mпользователь\033[33;3m: \033[0m"))
+    this.conn.Write([]byte("\033[34;1m用户名\033[33;3m: \033[0m"))
     username, err := this.ReadLine(false)
     if err != nil {
         return
@@ -43,7 +43,7 @@ func (this *Admin) Handle() {
 
     // Get password
     this.conn.SetDeadline(time.Now().Add(60 * time.Second))
-    this.conn.Write([]byte("\033[34;1mпароль\033[33;3m: \033[0m"))
+    this.conn.Write([]byte("\033[34;1m密码\033[33;3m: \033[0m"))
     password, err := this.ReadLine(true)
     if err != nil {
         return
@@ -53,15 +53,15 @@ func (this *Admin) Handle() {
     this.conn.Write([]byte("\r\n"))
     spinBuf := []byte{'-', '\\', '|', '/'}
     for i := 0; i < 15; i++ {
-        this.conn.Write(append([]byte("\r\033[37;1mпроверив счета... \033[31m"), spinBuf[i % len(spinBuf)]))
+        this.conn.Write(append([]byte("\r\033[37;1m检查账户... \033[31m"), spinBuf[i % len(spinBuf)]))
         time.Sleep(time.Duration(300) * time.Millisecond)
     }
 
     var loggedIn bool
     var userInfo AccountInfo
     if loggedIn, userInfo = database.TryLogin(username, password); !loggedIn {
-        this.conn.Write([]byte("\r\033[32;1mпроизошла неизвестная ошибка\r\n"))
-        this.conn.Write([]byte("\033[31mнажмите любую клавишу для выхода. (any key)\033[0m"))
+        this.conn.Write([]byte("\r\033[32;1m出现未知错误\r\n"))
+        this.conn.Write([]byte("\033[31m按任意键退出. (any key)\033[0m"))
         buf := make([]byte, 1)
         this.conn.Read(buf)
         return
